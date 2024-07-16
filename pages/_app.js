45ac8f6bf45a8e94231930bab306a8b1d51b213f
viswanathn <viswanathn252@gmail.com>
2024-07-16 09:06:08 +0530

    adding the changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'tailwindcss/tailwind.css'
-
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+<ContentfulLivePreviewProvider locale='en-US' enableLiveUpdates={true}>
+  
+<Component {...pageProps} />
+</ContentfulLivePreviewProvider>
+  ) 
 }
 
 export default MyApp
