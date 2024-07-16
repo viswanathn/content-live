6ae83949b387d12d40912d17019f1c0fa918b7a2
viswanathn <viswanathn252@gmail.com>
2024-07-16 08:17:13 +0530

    adding the chnages

---
+++ components/config.js
@@ -0,0 +1,10 @@
+// All environment variables will be sourced
+// Do NOT commit this file to source control
+module.exports = {
+    space_id: 'dukqjjlepny4',
+    delivery_token: 'IQJDMhDq-tQK7B1SNndE2FksKktws6dDTnSYwbjT9FA',
+    preview_token: 'OqFzaUCcBWTTfeso7_-q2C0YajclqYGeaX9uQwaGY-M',
+    previewSecret: 'OqFzaUCcBWTTfeso7_-q2C0YajclqYGeaX9uQwaGY-M',
+    environment: 'test-json-poc'
+    }
+    
